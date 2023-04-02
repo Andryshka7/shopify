7b27507184067d21e8bc7fc4cad4afa1861fea7d
Andrey Lapchik <a.lapchik7@gmail.com>
2023-04-02 10:24:18 +0300

    fix

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import filtersIcon from 'assets/filters-icon.png'
 import Filters from './Filters/Filters'
-import useFilteredProducts from 'hooks/useFilterProducts'
-import './products.css'
+// import useFilteredProducts from 'hooks/useFilterProducts'
 import ImageContainer from 'components/image container/ImageContainer'
+import './products.css'
+import { useAppDispatch, useAppSelector } from 'hooks/storehooks'
 
 const Products = () => {
 	const [open, setOpen] = useState<boolean>(true)
 	const switchOpen = () => setOpen((p) => !p)
-	const products = useFilteredProducts()
+	// const products = useFilteredProducts()
+	const { products } = useAppSelector((store) => store)
 
 	return (
 		<div className='products-container'>
